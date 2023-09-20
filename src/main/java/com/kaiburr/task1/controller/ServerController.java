fefd0cbe572b7c03ea5d47bf853271cc3a2dbd7a
Oeyshik Das <oeyshikdas1853@gmail.com>
2023-09-21 01:21:41 +0530

    Added Delete all servers API request

--- src/main/java/com/kaiburr/task1/controller/ServerController.java
+++ src/main/java/com/kaiburr/task1/controller/ServerController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/servers")
 public class ServerController {
@@ -64,12 +65,17 @@ public class ServerController {
         }
     }
 
-
-
     // Delete server by ID
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteServer(@PathVariable String id) {
         serverRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // Delete all servers
+    @DeleteMapping("/all")
+    public ResponseEntity<Void> deleteAllServers() {
+        serverRepository.deleteAll();
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
